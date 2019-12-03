f011da63d5146fc0d82fa083b93e341bf34709e5
Sophie <svdeeren@hotmail.com>
2019-12-03 01:31:14 +0100

    day 2 part 2

--- Python/Day2/Intcode.py
+++ Python/Day2/Intcode.py
@@ -1,17 +1,25 @@
 with open("input.txt") as file:
-    L = list(map(int, file.readline().split(",")))
-    for i in range(0, len(L), 4):
-        opc = L[i]
-        if opc == 1:
-            pos1 = L[i + 1]
-            pos2 = L[i + 2]
-            L[L[i + 3]] = L[pos1] + L[pos2]
-        elif opc == 2:
-            pos1 = L[i + 1]
-            pos2 = L[i + 2]
-            L[L[i + 3]] = L[pos1] * L[pos2]
-        elif opc == 99:
-            print(L[0])
-            exit()
-        else:
-            raise Exception("unknown opcode")
+    og = list(map(int, file.readline().split(",")))
+
+    for k in range(0, 100):
+        for j in range(0, 100):
+            L = og.copy()
+            L[1] = k
+            L[2] = j
+            for i in range(0, len(L), 4):
+                opc = L[i]
+                if opc == 1:
+                    pos1 = L[i + 1]
+                    pos2 = L[i + 2]
+                    L[L[i + 3]] = L[pos1] + L[pos2]
+                elif opc == 2:
+                    pos1 = L[i + 1]
+                    pos2 = L[i + 2]
+                    L[L[i + 3]] = L[pos1] * L[pos2]
+                elif opc == 99:
+                    if L[0] == 19690720:
+                        print('done')
+                        print(100*L[1] + L[2])
+                        exit()
+                else:
+                    break
