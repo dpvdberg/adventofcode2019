39595d2b6128440e603799f3daaa5facb480d374
Sophie <svdeeren@hotmail.com>
2019-12-04 01:16:00 +0100

    day 3 part 2

--- Python/Day3/CrossedWires.py
+++ Python/Day3/CrossedWires.py
@@ -2,11 +2,12 @@ d = {}
 col = []
 
 
-def check_collision(curPos, i):
+def check_collision(curPos, i, step):
     if curPos in d:
-        if d[curPos] != i:
-            col.append(curPos)
-    d[curPos] = i
+        if d[curPos][0] != i:
+            col.append(step + d[curPos][1])
+    else:
+        d[curPos] = (i, step)
 
 
 with open("input.txt") as file:
@@ -15,27 +16,32 @@ i = 0
 for line in lines:
     l = line.split(",")
     curPos = (0, 0)
+    step = 0
     for x in l:
         char = x[0]
         value = int(x[1:])
         if char == "R":
             for j in range(curPos[0] + 1, curPos[0] + value + 1):
                 curPos = (j, curPos[1])
-                check_collision(curPos, i)
+                step = step + 1
+                check_collision(curPos, i, step)
         elif char == "L":
             for j in range(curPos[0] - 1, curPos[0] - value - 1, -1):
                 curPos = (j, curPos[1])
-                check_collision(curPos, i)
+                step = step + 1
+                check_collision(curPos, i, step)
         elif char == "U":
             for j in range(curPos[1] + 1, curPos[1] + value + 1):
                 curPos = (curPos[0], j)
-                check_collision(curPos, i)
+                step = step + 1
+                check_collision(curPos, i, step)
         elif char == "D":
             for j in range(curPos[1] - 1, curPos[1] - value - 1, -1):
                 curPos = (curPos[0], j)
-                check_collision(curPos, i)
+                step = step + 1
+                check_collision(curPos, i, step)
         else:
             raise Exception("Illegal direction")
     print(curPos)
     i = i + 1
-print(min(list(map(lambda y: abs(y[0]) + abs(y[1]), col))))
+print(min(col))
