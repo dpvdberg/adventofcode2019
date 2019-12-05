c8d449e09d63888d04f3e69be9dfae298d0c91fc
Sophie <svdeeren@hotmail.com>
2019-12-05 19:38:16 +0100

    day 4 part 2

--- Python/Day4/SecureContainer.py
+++ Python/Day4/SecureContainer.py
@@ -3,9 +3,14 @@ for i in range(307237, 769059):
     adjacent = False
     decreasing = False
     l = list(map(int, str(i)))
-    for j in range(0,len(l)-1):
+    for j in range(0, len(l)-1):
         if l[j] == l[j + 1]:
-            adjacent = True
+            if j == len(l)-2 and  l[j-1] != l[j]:
+                adjacent = True
+            if j == 0 and l[j+1] != l[j+2]:
+                adjacent = True
+            if j != len(l)-2 and j != 0 and l[j+1] != l[j+2] and l[j-1] != l[j]:
+                adjacent = True
         if l[j] > l[j+1]:
             decreasing = True
             break
