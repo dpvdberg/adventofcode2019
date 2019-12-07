16be5d09901060aa8fad551809fa99a6d3fb9583
Dennis van den Berg <dennispvandenberg@gmail.com>
2019-12-07 13:32:02 +0100

    day 6 part 2

--- Python/Day6/orbit.py
+++ Python/Day6/orbit.py
@@ -1,13 +1,36 @@
+from operator import itemgetter
+
+
 def orbitals(m, point):
     if point in m:
         orbit_point = m[point]
-        return 1 + orbitals(m, orbit_point)
+        l = [orbit_point]
+        l.extend(orbitals(m, orbit_point))
+        return l
     else:
-        return 0
+        return []
 
 
 def orbitals_sum(m):
-    return sum([orbitals(m, x) for x in m.keys()])
+    return sum([len(orbitals(m, x)) for x in m.keys()])
+
+
+def orbitals_intersect(m, p1, p2):
+    l1 = orbitals(m, p1)
+    l2 = orbitals(m, p2)
+    return [x for x in l1 if x in l2]
+
+
+def closest_orbital_intersect(m, p1, p2):
+    intersect = orbitals_intersect(m, p1, p2)
+    z = zip(intersect, [len(orbitals(m, x)) for x in intersect])
+    return max(z, key=itemgetter(1))[0]
+
+
+def orbitals_path_length(m, p1, p2):
+    closest_intersection = closest_orbital_intersect(m, p1, p2)
+    intersection_len = len(orbitals(m, closest_intersection))
+    return len(orbitals(m, p1)) - intersection_len + len(orbitals(m, p2)) - intersection_len
 
 
 def orbitals_map(file_name="input.txt"):
@@ -23,4 +46,4 @@ def orbitals_map(file_name="input.txt"):
     return m
 
 
-print(orbitals_sum(orbitals_map()))
+print(orbitals_path_length(orbitals_map(), "YOU", "SAN") - 2)
