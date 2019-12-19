0b851cb7e533c91598d71882a36c0d10d8ef2191
Sophie <svdeeren@hotmail.com>
2019-12-19 21:21:03 +0100

    day 13 part 1

---
+++ Python/Solutions/Day13/SpacePolice.py
@@ -0,0 +1,52 @@
+from Solutions.Day9.Intcode import IntCode
+import math
+from collections import defaultdict
+from operator import add
+
+
+class FeedbackAmplifier2(IntCode):
+    pending_input = None
+    pending_output = None
+
+    def __init__(self, file_name="input.txt"):
+        IntCode.__init__(self, file_name)
+
+    def provide_input(self):
+        if self.pending_input:
+            return self.pending_input.pop()
+
+        raise Exception("No input...")
+
+    def provide_output(self, o):
+        self.pending_output = o
+
+    def input_to_output(self, feedback_input):
+        self.pending_input = feedback_input
+        self.pending_output = None
+
+        while self.IP < len(self.lst):
+            if self.instruction_step():
+                return None
+
+            if self.pending_output is not None:
+                return self.pending_output
+
+
+def main():
+    comp = FeedbackAmplifier2()
+    game = []
+    count = 0
+    while True:
+        x = comp.input_to_output(None)
+        #print(x)
+        if x is None:
+            break
+        y = comp.input_to_output(None)
+        title_id = comp.input_to_output(None)
+        if title_id == 2:
+            count = count + 1
+        #game.append((x, y, title_id))
+    print(count)
+
+
+main()
