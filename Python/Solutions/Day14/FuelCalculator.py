8bdebd58661c9db5562455ea8e03f3a4b0a36bfb
Sophie <svdeeren@hotmail.com>
2019-12-20 22:57:19 +0100

    day 14 part 1

--- Python/Solutions/Day14/FuelCalculator.py
+++ Python/Solutions/Day14/FuelCalculator.py
@@ -1,27 +1,41 @@
+import math
+from collections import defaultdict
+
+
 def sanitize_input(filename='input.txt'):
     production_rules = {}
     with open(filename) as file:
         for line in file:
             from_to = line.split("=>")
             ingredients = from_to[0].strip().split(",")
-            ingredients = {x.split()[1] : int(x.split()[0]) for x in ingredients}
+            ingredients = {x.split()[1]: int(x.split()[0]) for x in ingredients}
             (result_count, result_ingredient) = from_to[1].strip().split()
             production_rules[result_ingredient] = (int(result_count), ingredients)
 
     return production_rules
 
-def calc_ore_needs(result, result_count, production_rules):
-    (count, ingredients) = production_rules[result]
-    # TODO return number of ores needed to produce result_count results
 
-    # if only ores needed for result return ores
-    # else
-    # TODO: ongeveer zoiets:
-    # ores needed = result_count / count * sum over ingredients : ingredient_count * calc_ore_needs(ingredient, production_rules)
-    pass
+def calc_ore_needs(result, result_count, production_rules, spare):
+    (count, ingredients) = production_rules[result]
+    rule_mult = math.ceil(result_count / count)
+    if "ORE" in ingredients:
+        spare[result] = spare[result] + (rule_mult * count) - result_count
+        return rule_mult * ingredients["ORE"]
+
+    ore_count = 0
+    for ingredient in ingredients:
+        amount = ingredients[ingredient] * rule_mult - spare[ingredient]
+        spare[ingredient] = abs(amount) if amount < 0 else 0
+        if amount < 0:
+            continue
+        ore_count = ore_count + calc_ore_needs(ingredient, amount, production_rules, spare)
+        spare[result] = spare[result] + (rule_mult * count) - result_count
+    return ore_count
+
 
 def part1(filename='input.txt'):
-    print(sanitize_input(filename))
+    rules = sanitize_input(filename)
+    print(calc_ore_needs("FUEL", 1, rules, defaultdict(lambda: 0)))
 
 
 part1()
